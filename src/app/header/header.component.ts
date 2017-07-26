7382b12db40268312626cba2faee159809a0d925
Artemiy Kondakov <artkon@gmail.com>
2017-07-26 08:34:08 +0300

    Init commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.pug',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+    // Do stuff
+  }
+
+  ngOnInit() {
+    console.log('Header component');
+  }
+
+}
