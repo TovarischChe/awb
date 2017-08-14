b31c8ba4f4eafe6fbf1e9b9dfccd4a170d818e58
Artemiy Kondakov <akondakov@condenast.ru>
2017-08-14 10:12:35 +0300

    Use pages array on list page

--- src/app/components/news-list/news-list.component.ts
+++ src/app/components/news-list/news-list.component.ts
@@ -1,177 +1,102 @@
 import { Component, OnInit } from '@angular/core';
 import { ScrollService } from '../../shared/scroll.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'ac-news-list',
   templateUrl: './news-list.component.pug'
 })
 export class NewsListComponent implements OnInit {
-
-  news = [
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
+  pages = [];
+  page = {
+    page: null,
+    items: [
+      {
+        code: 'new-phantom',
+        name: 'Представлен новый Rolls-Royce Phantom',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
+        }
+      },
+      {
+        code: 'borgward-isabella',
+        name: 'Марка Borgward вернет классическую модель «Изабелла»',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
+        }
+      },
+      {
+        code: 'new-phantom',
+        name: 'Представлен новый Rolls-Royce Phantom',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
+        }
+      },
+      {
+        code: 'borgward-isabella',
+        name: 'Марка Borgward вернет классическую модель «Изабелла»',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
+        }
+      },
+      {
+        code: 'new-phantom',
+        name: 'Представлен новый Rolls-Royce Phantom',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
+        }
+      },
+      {
+        code: 'borgward-isabella',
+        name: 'Марка Borgward вернет классическую модель «Изабелла»',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
+        }
+      },
+      {
+        code: 'new-phantom',
+        name: 'Представлен новый Rolls-Royce Phantom',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
+        }
+      },
+      {
+        code: 'borgward-isabella',
+        name: 'Марка Borgward вернет классическую модель «Изабелла»',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
+        }
+      },
+      {
+        code: 'new-phantom',
+        name: 'Представлен новый Rolls-Royce Phantom',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
+        }
+      },
+      {
+        code: 'borgward-isabella',
+        name: 'Марка Borgward вернет классическую модель «Изабелла»',
+        image: {
+          url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
+        }
       }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    },
-    {
-      code: 'new-phantom',
-      name: 'Представлен новый Rolls-Royce Phantom',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/28/07/1350927/05adabf65dd6cbf720122681378c6ff521dc3358.jpg'
-      }
-    },
-    {
-      code: 'borgward-isabella',
-      name: 'Марка Borgward вернет классическую модель «Изабелла»',
-      image: {
-        url: 'https://motor.ru/imgs/2017/07/27/14/1350681/3d049dabc9149a4fab767b38d29b4fb83b277461.jpg'
-      }
-    }
-  ];
+    ]
+  };
 
   constructor(private scrollService: ScrollService) {
-    this.scrollService.$WindowScrolled.subscribe((data) => {
-      console.log(data);
+    this.scrollService.$WindowScrolledLeft10.subscribe(() => {
+      console.log('10% left, push new page');
+      let newPage = _.clone(this.page);
+      newPage.page = this.pages.length + 1;
+      this.pages.push(newPage);
     });
   }
 
   ngOnInit() {
-    // console.log('Header component');
+    let newPage = _.clone(this.page);
+    newPage.page = this.pages.length + 1;
+    this.pages.push(newPage);
   }
 
 }
